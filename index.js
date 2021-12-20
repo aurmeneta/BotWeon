d04ab0e9fd3232a36626f9d1191b929ad71b5908
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2021-12-20 20:07:14 -0300

    feat: top más weones

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { Database } = require("./src/Database")
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
+// Contador de invocaciones
+let contador = 0
+
 // Conectar a la db y empezar el cliente
 const db = new Database();
 (async () => {
@@ -30,14 +33,42 @@ client.on("messageCreate", async (message) => {
 
     // Actualizar count del usuario
     const { id: uid } = message.author
-    const server_id = message.guild.id
-    const usuario = await db.buscarUsuario(uid, server_id)
+    const serverId = message.guild.id
+    let usuario = await db.buscarUsuario(uid, serverId)
     usuario.count++
-    await db.actualizarUsuario(usuario, server_id)
+    await db.actualizarUsuario(usuario, serverId)
 
     // Responder Mensaje
     const cantidad_respuestas = config.replies.length
     const respuesta = config.replies[(usuario.count - 1) % cantidad_respuestas]
     message.reply(respuesta)
+
+    // Mostrar Top más weones
+    if (contador % 10 == 0) {
+        const top = await db.top(serverId, 5)
+
+        if (top.length < 3) return
+
+        // Obtener nombres de usuario
+        const promisesUsuarios = []
+        top.forEach(({ id }) => promisesUsuarios.push(client.users.fetch(id)))
+        const usuariosDiscord = await Promise.all(promisesUsuarios)
+
+        // 
+        usuariosDiscord.forEach((usuarioDiscord, idx) => {
+            top[idx].nombre = usuarioDiscord.username
+        })
+
+        let mensajeTop = ":100: TOP MÁS WEONES DEL SERVER :100:\n\n"
+        top.forEach((usuario, idx) => {
+            mensajeTop += `${idx + 1}.\t ${usuario.nombre} - ${usuario.count} veces\n`
+        })
+
+        mensajeTop += "\nSigue siendo wn para aparecer en este top :star_struck:"
+
+        message.channel.send(mensajeTop)
+    }
+
+    contador += 1
 })
 
--- src/Database.js
+++ src/Database.js
@@ -15,31 +15,38 @@ class Database {
             `CREATE TABLE IF NOT EXISTS SID_${name} (
                 id TEXT PRIMARY KEY,
                 count INTEGER DEFAULT 0
-            )`)
+            );`)
     }
 
-    async buscarUsuario(uid, server_id) {
-        await this.createTable(server_id)
+    async buscarUsuario(uid, serverId) {
+        await this.createTable(serverId)
 
-        let resultado = await this.db.get(`SELECT * FROM SID_${server_id} WHERE id = ${uid}`)
+        let resultado = await this.db.get(`SELECT * FROM SID_${serverId} WHERE id = ${uid};`)
 
         if (resultado) return resultado
         else {
-            await this.crearUsuario(uid, server_id)
-            return await this.buscarUsuario(uid, server_id)
+            await this.crearUsuario(uid, serverId)
+            return await this.buscarUsuario(uid, serverId)
         }
     }
 
-    async crearUsuario(uid, server_id) {
-        await this.db.exec(`INSERT INTO SID_${server_id} (id)
+    async crearUsuario(uid, serverId) {
+        await this.db.exec(`INSERT INTO SID_${serverId} (id)
                             VALUES (${uid});`)
     }
 
-    async actualizarUsuario(usuario, server_id) {
+    async actualizarUsuario(usuario, serverId) {
         const { id, count } = usuario
-        await this.db.exec(`UPDATE SID_${server_id}
+        await this.db.exec(`UPDATE SID_${serverId}
                             SET count = ${count}
-                            WHERE id = ${id}`)
+                            WHERE id = ${id};`)
+    }
+
+    async top(serverId, cantidad) {
+        const list = await this.db.all(`SELECT * from SID_${serverId}
+                                        ORDER BY count DESC
+                                        LIMIT ${cantidad};`)
+        return list
     }
 
 
